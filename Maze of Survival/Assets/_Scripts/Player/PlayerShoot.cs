e5cb58d1013db7631de58a2c9f986eba057eeed7
Dylan <changliangdylan@gmail.com>
2025-02-17 08:44:38 -0500

    Level Sky
    - Added skybox
    - Player animation

--- Maze of Survival/Assets/_Scripts/Player/PlayerShoot.cs
+++ Maze of Survival/Assets/_Scripts/Player/PlayerShoot.cs
@@ -1,33 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public GameObject bulletPrefab;   // Bullet prefab to be instantiated
-    public Transform playerCamera;    // The player's camera for direction
-    public Transform playerHand;      // The player's hand position to spawn the bullet
-    public float throwForce = 10f;    // The force applied to the bullet
+    public GameObject bulletPrefab;
+    public Transform playerCamera;
+    public Transform playerHand;
+    public Animator playerAnimator;
+    public float throwForce = 10f;
+    public float cooldown = 0.5f;
+
+    private bool canShoot = true;
 
     void Update()
     {
-        // Shoot when the left mouse button is pressed
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canShoot)
         {
-            ThrowBullet();
+            StartCoroutine(ShootWithCooldown());
         }
+    }
+
+    IEnumerator ShootWithCooldown()
+    {
+        canShoot = false;
+        ThrowBullet();
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
     }
 
     void ThrowBullet()
     {
-        // Instantiate the bullet at the player's hand position
-        // This will spawn the bullet slightly in front of the hand and align it with the camera's forward direction
         GameObject bullet = Instantiate(bulletPrefab, playerHand.position, playerCamera.rotation);
-
-        // Get the Rigidbody component of the bullet
         Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+
+        playerAnimator.SetTrigger("shoot");
 
         if (bulletRb != null)
         {
-            // Apply force to the bullet in the direction the camera is facing
             bulletRb.AddForce(playerCamera.forward * throwForce, ForceMode.VelocityChange);
         }
     }
