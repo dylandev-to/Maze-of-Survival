0e7ad7e68b9bc1e156171bc0f61df80ceca75e2e
Dylan <changliangdylan@gmail.com>
2025-02-18 17:27:29 -0500

    Added winning screen

--- Maze of Survival/Assets/_Scripts/Quest/Key.cs
+++ Maze of Survival/Assets/_Scripts/Quest/Key.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
+    public AudioSource collectFX;
+    
+    private bool _collected;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             QuestManager questManager = FindObjectOfType<QuestManager>();
-            if (questManager != null)
+            if (questManager != null && _collected == false)
             {
+                collectFX.Play();
+                _collected = true;
                 questManager.CollectKey();
-                Destroy(gameObject); // Remove the key
+                Destroy(gameObject, 1);
             }
         }
     }
