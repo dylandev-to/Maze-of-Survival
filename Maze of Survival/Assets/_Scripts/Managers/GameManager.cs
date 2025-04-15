6f3f6c739ac60e0b61f83e983e31085062fbe030
Dylan <changliangdylan@gmail.com>
2025-04-15 19:01:06 -0400

    Last Fix

--- Maze of Survival/Assets/_Scripts/Managers/GameManager.cs
+++ Maze of Survival/Assets/_Scripts/Managers/GameManager.cs
@@ -5,19 +5,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
